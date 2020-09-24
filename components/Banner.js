d21b5afaaa04cb385f5b8f2056ff9f8b44fc5792
Nenad Marinkovic <nenadmarinkovic@protonmail.com>
2020-09-24 15:37:21 +0200

    layout fixed

--- components/Banner.js
+++ components/Banner.js
@@ -21,16 +21,17 @@ function Banner(props) {
         <BannerWrapper>
           <Header theme={props.theme} toggleTheme={props.toggleTheme} />
           <BannerContainer>
-            <Fade>
-              <Info>
+            <Info>
+              <Fade>
                 <Title>Hi. I'm Nenad, a web developer.</Title>
                 <Subtitle>
                   I create websites and web applications that are fast, clean,
                   and enjoyable.
                 </Subtitle>
                 <ContactButton />
-              </Info>
-            </Fade>
+              </Fade>
+            </Info>
+
             <Illustration />
           </BannerContainer>
         </BannerWrapper>
