5d5b20bec46da5ecdb65c334f1e5ee313680344a
Nenad Marinkovic <nenadmarinkovic@protonmail.com>
2020-09-25 12:48:16 +0200

    faster server optimized

--- pages/_document.js
+++ pages/_document.js
@@ -1,22 +1,34 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class WebSite extends Document {
   static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps }
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
   }
 
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+          <meta name="Personal Web Development and Design Portfolio"></meta>
+          <link
+            rel="stylesheet"
+            type="text/css"
+            charSet="UTF-8"
+            href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
+          />{" "}
+        </Head>
         <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
 
-export default WebSite
+export default WebSite;
--- pages/index.js
+++ pages/index.js
@@ -4,16 +4,8 @@ import Main from "../components/Main";
 export default function Home() {
   return (
     <>
-      <Head >
+      <Head>
         <title>Nenad Marinković</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta name="Personal Web Development and Design Portfolio"></meta>
-        <link
-          rel="stylesheet"
-          type="text/css"
-          charSet="UTF-8"
-          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
-        />
       </Head>
       <Main />
     </>
