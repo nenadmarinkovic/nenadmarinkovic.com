00ffc13dead8f0c0984c49913d098dd52686c289
Nenad <nenadmarinkovic@protonmail.com>
2020-09-28 21:23:41 +0200

    smooth scroll mobile

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
+import * as smoothscroll from "smoothscroll-polyfill";
 import Head from "next/head";
 import Main from "../components/Main";
-import * as smoothscroll from 'smoothscroll-polyfill';
 smoothscroll.polyfill();
 
 export default function Home() {
