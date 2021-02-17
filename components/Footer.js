eed2b2c70699f6cbb21488a4d942dc2618e271e2
Nenad Marinkovic <nenadmarinkovic@protonmail.com>
2021-02-17 08:02:47 +0100

    svg footer flickering solved

--- components/Footer.js
+++ components/Footer.js
@@ -13,10 +13,7 @@ function Footer() {
     <>
       <Wave>
         <WaveSVG xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-          <path
-           
-            d="M0,0L80,10.7C160,21,320,43,480,58.7C640,75,800,85,960,85.3C1120,85,1280,75,1360,69.3L1440,64L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"
-          ></path>
+          <path d="M0,0L80,10.7C160,21,320,43,480,58.7C640,75,800,85,960,85.3C1120,85,1280,75,1360,69.3L1440,64L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path>
         </WaveSVG>
       </Wave>
       <FooterContainer>
@@ -25,7 +22,10 @@ function Footer() {
             <PortfolioIntroMain id="contact">Contact</PortfolioIntroMain>
             <PortfolioIntroSecond>
               If you'd like to contact me, you can send me an email to {""}
-              <a className="a-link" href="mailto:nenadmarinkovic@protonmail.com">
+              <a
+                className="a-link"
+                href="mailto:nenadmarinkovic@protonmail.com"
+              >
                 nenadmarinkovic@protonmail.com
               </a>
               <br />
