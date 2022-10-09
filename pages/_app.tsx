f22c749f96e6bc42477b871814e01261b0663dac
nenadmarinkovic <nenadmarinkovic@protonmail.com>
2022-10-09 15:35:47 +0200

    analytics test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,25 @@
 import type { AppProps } from "next/app";
-
+import Script from "next/script";
 import "../styles/styles.css";
 
 export default function Website({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-54KLXK121W"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){window.dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-54KLXK121W');
+    `}
+      </Script>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
