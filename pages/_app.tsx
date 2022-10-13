e59f1739eb9a1f36d6d595a86a7dcf0b40d09ba1
nenadmarinkovic <nenadmarinkovic@protonmail.com>
2022-10-13 22:39:49 +0200

    gtag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,7 +13,7 @@ export default function Website({ Component, pageProps }: AppProps) {
               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
               'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer', 'G-54KLXK121W');
+              })(window,document,'script','dataLayer', '54KLXK121W');
             `,
         }}
       />
