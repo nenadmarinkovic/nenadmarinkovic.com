7536f7687e5720e11d6482e09a6e28001ed7c31d
nenadmarinkovic <nenadmarinkovic@protonmail.com>
2022-10-13 22:27:37 +0200

    gtag testing

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,24 +1,22 @@
 import type { AppProps } from "next/app";
+import Script from "next/script";
 
 export default function Website({ Component, pageProps }: AppProps) {
   return (
     <>
-      {/* <Script
+      <Script
+        id="analytics"
         strategy="afterInteractive"
-        src="https://www.googletagmanager.com/gtag/js?id=G-54KLXK121W"
+        dangerouslySetInnerHTML={{
+          __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer', 'G-54KLXK121W');
+            `,
+        }}
       />
-      <Script id="google-analytics" strategy="afterInteractive">
-        
-        {`  
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){window.dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', 'G-54KLXK121W');
-
-            
-        `}
-      </Script> */}
 
       <Component {...pageProps} />
     </>
