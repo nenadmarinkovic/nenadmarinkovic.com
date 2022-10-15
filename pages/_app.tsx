215747bbbb1099f8e1d915161ea1e807f4337b30
nenadmarinkovic <nenadmarinkovic@protonmail.com>
2022-10-15 22:57:28 +0200

    gtag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,10 @@ import Script from "next/script";
 export default function Website({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-0K713VRZP5`}
+      />
       <Script id="google-analytics" strategy="afterInteractive">
         {`
           window.dataLayer = window.dataLayer || [];
