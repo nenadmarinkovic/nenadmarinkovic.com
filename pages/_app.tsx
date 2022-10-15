338d0632465bad5a35e83c59732fa2da2676a673
nenadmarinkovic <nenadmarinkovic@protonmail.com>
2022-10-15 22:19:21 +0200

    gtag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,20 +8,7 @@ export default function Website({ Component, pageProps }: AppProps) {
         strategy="afterInteractive"
         src={`https://www.googletagmanager.com/gtag/js?id=G-0K713VRZP5`}
       />
-      <Script
-        id="gtag-init"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-        window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-        
-          gtag('config', 'G-0K713VRZP5', {
-            page_path: window.location.pathname
-          });`,
-        }}
-      />
+  
       <Component {...pageProps} />
     </>
   );
