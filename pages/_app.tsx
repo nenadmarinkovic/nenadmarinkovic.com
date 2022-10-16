15b974c54aaf6acec2816c3425bb47c5e7baf867
nenadmarinkovic <nenadmarinkovic@protonmail.com>
2022-10-16 17:57:16 +0200

    gtag updated

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,30 +1,24 @@
 import type { AppProps } from "next/app";
-import { useEffect } from "react";
-import { useRouter } from "next/router";
 import Script from "next/script";
-import { pageview } from "../lib/gtm";
 
 export default function Website({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-  useEffect(() => {
-    router.events.on("routeChangeComplete", pageview);
-    return () => {
-      router.events.off("routeChangeComplete", pageview);
-    };
-  }, [router.events]);
   return (
     <>
-      {/* Google Tag Manager - Global base code */}
       <Script
-        id="gtag-base"
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-0K713VRZP5`}
+      />
+      <Script
+        id="gtag-init"
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `
-          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer', 'G-0K713VRZP5');
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-0K713VRZP5', {
+            page_path: window.location.pathname,
+          });
         `,
         }}
       />
