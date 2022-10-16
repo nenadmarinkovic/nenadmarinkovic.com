ba11f13d2aa89b5e6056e7ee2d21462011657f76
nenadmarinkovic <nenadmarinkovic@protonmail.com>
2022-10-16 18:20:35 +0200

    testing speed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,24 +3,6 @@ import type { AppProps } from "next/app";
 export default function Website({ Component, pageProps }: AppProps) {
   return (
     <>
-      {/* <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=G-0K713VRZP5`}
-      />
-      <Script
-        id="gtag-init"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-0K713VRZP5', {
-            page_path: window.location.pathname,
-          });
-        `,
-        }}
-      /> */}
       <Component {...pageProps} />
     </>
   );
