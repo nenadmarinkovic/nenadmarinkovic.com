8a592205ed0f3775e91e16f53e30f8c197d9a443
nenadmarinkovic <nenadmarinkovic@protonmail.com>
2022-10-16 18:16:10 +0200

    del gtag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,9 @@
 import type { AppProps } from "next/app";
-import Script from "next/script";
 
 export default function Website({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Script
+      {/* <Script
         strategy="afterInteractive"
         src={`https://www.googletagmanager.com/gtag/js?id=G-0K713VRZP5`}
       />
@@ -21,7 +20,7 @@ export default function Website({ Component, pageProps }: AppProps) {
           });
         `,
         }}
-      />
+      /> */}
       <Component {...pageProps} />
     </>
   );
