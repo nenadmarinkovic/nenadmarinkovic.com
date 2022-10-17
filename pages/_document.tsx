5e5f74b2e5bd24f6f87e22db29224b31f38ad65d
nenadmarinkovic <nenadmarinkovic@protonmail.com>
2022-10-17 22:27:08 +0200

    doc update

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,7 +1,14 @@
-import Document, { DocumentContext } from "next/document";
+import Document, {
+  DocumentContext,
+  Html,
+  Head,
+  Main,
+  NextScript,
+} from "next/document";
+
 import { ServerStyleSheet } from "styled-components";
 
-export default class Doc extends Document {
+class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
@@ -14,13 +21,32 @@ export default class Doc extends Document {
         });
 
       const initialProps = await Document.getInitialProps(ctx);
-
       return {
         ...initialProps,
-        styles: [initialProps.styles, sheet.getStyleElement()],
+
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
       };
     } finally {
       sheet.seal();
     }
   }
+
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+export default MyDocument;
