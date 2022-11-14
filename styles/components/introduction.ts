6e46e724351ab4261724229deced73c0fd8dfe21
nenadmarinkovic <nenadmarinkovic@protonmail.com>
2022-11-14 09:17:47 +0100

    intro layout

--- styles/components/introduction.ts
+++ styles/components/introduction.ts
@@ -4,7 +4,7 @@ export const Introduction = styled.p`
   color: ${({ theme }) => theme.color};
   font-size: 1.25rem;
   line-height: 1.9;
-  width: 76%;
+  width: 78.5%;
 
   @media (max-width: 48rem) {
     margin-top: 1rem;
