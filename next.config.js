12ec560a02843762a83e9527387403095290f36e
nenadmarinkovic <nenadmarinkovic@protonmail.com>
2022-11-15 18:59:52 +0100

    config change

--- next.config.js
+++ next.config.js
@@ -59,23 +59,7 @@ module.exports = {
   },
 };
 
-const ContentSecurityPolicy = `
-    default-src 'self';
-    script-src 'self' 'unsafe-eval' 'unsafe-inline' *.youtube.com *.twitter.com;
-    child-src *.youtube.com *.google.com *.twitter.com;
-    style-src 'self' 'unsafe-inline' *.googleapis.com;
-    img-src * blob: data:;
-    media-src 'none';
-    connect-src *;
-    font-src 'self';
-`;
-
 const securityHeaders = [
-  {
-    key: "Content-Security-Policy",
-    value: ContentSecurityPolicy.replace(/\n/g, ""),
-  },
-
   {
     key: "Referrer-Policy",
     value: "origin-when-cross-origin",
--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,54 +1,31 @@
 import Head from "next/head";
-import fs from "fs";
-import matter from "gray-matter";
-import path from "path";
 import type { NextPage } from "next";
 import { ThemeLayout } from "../styles/components/layout";
 import { Introduction } from "../styles/components/introduction";
-import { postFilePaths, POSTS_PATH } from "../utils/mdx-posts";
 import Footer from "../components/Footer";
 import { Container } from "../styles/components/layout";
 import Header from "../components/Header";
 import Banner from "../components/Banner";
 
-const ContactPage: NextPage = ({
-  spotifyData,
-  theme,
-  toggleTheme,
-}: any) => {
+const ContactPage: NextPage = ({ spotifyData, theme, toggleTheme }: any) => {
   return (
     <>
       <Head>
-        <title>Nenad Marinković | Notes</title>
+        <title>Nenad Marinković | Contact</title>
       </Head>
       <ThemeLayout>
         <Header toggleTheme={toggleTheme} theme={theme} />
         <Container>
           <Banner name="Contact" />
           <Introduction>
-           You can contact me through the form or at: nenadmarinkovic@protonmail.com
+            You can contact me through the form or at:
+            nenadmarinkovic@protonmail.com
           </Introduction>
-         
         </Container>
         <Footer spotifyData={spotifyData} />
       </ThemeLayout>
     </>
   );
 };
-
-export function getStaticProps() {
-  const posts = postFilePaths.map((filePath) => {
-    const source = fs.readFileSync(path.join(POSTS_PATH, filePath));
-    const { content, data } = matter(source);
-
-    return {
-      content,
-      data,
-      filePath,
-    };
-  });
-
-  return { props: { posts } };
-}
 
 export default ContactPage;
