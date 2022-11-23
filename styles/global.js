8af91a93b1008ca1feebb6155f3fe34f3e8a7212
nenadmarinkovic <nenadmarinkovic@protonmail.com>
2022-11-23 20:21:12 +0100

    layout change

--- styles/global.js
+++ styles/global.js
@@ -38,6 +38,7 @@ export const GlobalStyle = createGlobalStyle`
       font-family: "Cera Pro", -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
         Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
       scrollbar-gutter: stable;
+      overflow: auto;
     
     }
     
