fe1930fd4b22ecc413369b42067c88a6937451d3
nenadmarinkovic <nenadmarinkovic@protonmail.com>
2022-11-24 22:38:10 +0100

    height adjusted

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -6,6 +6,7 @@ import Footer from "../components/Footer";
 import { Container } from "../styles/components/layout";
 import Header from "../components/Header";
 import Banner from "../components/Banner";
+import { HeightContent } from "../styles/pages/contact";
 
 const ContactPage: NextPage = ({ spotifyData, theme, toggleTheme }: any) => {
   return (
@@ -21,6 +22,7 @@ const ContactPage: NextPage = ({ spotifyData, theme, toggleTheme }: any) => {
             You can contact me through the form or at:
             nenadmarinkovic@protonmail.com
           </Introduction>
+          <HeightContent/>
         </Container>
         <Footer spotifyData={spotifyData} />
       </ThemeLayout>
---
+++ styles/pages/contact.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+// TODO: Change later
+
+export const HeightContent = styled.div`
+  height: 1000px;
+`;
