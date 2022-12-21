4886c52033fe5d91bae261b2bb7d1a230191db35
nenadmarinkovic <nenadmarinkovic@protonmail.com>
2022-12-21 22:10:06 +0100

    font speed testing

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -45,21 +45,18 @@ class Website extends Document {
             href="/fonts/CeraPro-Regular.woff2"
             as="font"
             type="font/woff2"
-            crossOrigin="crossorigin"
           />
           <link
             rel="preload"
             href="/fonts/CeraPro-Bold.woff2"
             as="font"
             type="font/woff2"
-            crossOrigin="crossorigin"
           />
           <link
             rel="preload"
             href="/fonts/CeraPro-Black.woff2"
             as="font"
             type="font/woff2"
-            crossOrigin="crossorigin"
           />
           <link rel="icon" href="/favicon.ico" />
           <link href="/manifest.json" rel="manifest" />
