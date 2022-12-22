7f2f108dca918fbef6590365619da7b9d30f4198
nenadmarinkovic <nenadmarinkovic@protonmail.com>
2022-12-22 07:48:32 +0100

    scroll header mobile

--- styles/global.js
+++ styles/global.js
@@ -39,6 +39,7 @@ export const GlobalStyle = createGlobalStyle`
       font-family: "Cera Pro", -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
         Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
       scrollbar-gutter: stable;
+      scroll-padding-top: 3rem;
     }
     
     input,
