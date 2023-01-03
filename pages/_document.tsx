7bfdf376bb96ef5f90fec0cf8b44019f400f8194
nenadmarinkovic <nenadmarinkovic@protonmail.com>
2023-01-03 10:45:39 +0100

    fonts preload

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -41,21 +41,21 @@ class Website extends Document {
         <Head>
           <meta name="description" content="Web development and design" />
           <link
-            rel="prefetch"
+            rel="preload"
             href="/fonts/CeraPro-Regular.woff2"
             as="font"
             type="font/woff2"
             crossOrigin="anonymous"
           />
           <link
-            rel="prefetch"
+            rel="preload"
             href="/fonts/CeraPro-Bold.woff2"
             as="font"
             type="font/woff2"
             crossOrigin="anonymous"
           />
           <link
-            rel="prefetch"
+            rel="preload"
             href="/fonts/CeraPro-Black.woff2"
             as="font"
             type="font/woff2"
