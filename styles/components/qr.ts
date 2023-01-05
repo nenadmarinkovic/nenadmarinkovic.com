71ae04156747f96977ba3ee177fa6a9d281ca671
nenadmarinkovic <nenadmarinkovic@protonmail.com>
2023-01-05 23:02:19 +0100

    qr layout change

--- styles/components/qr.ts
+++ styles/components/qr.ts
@@ -1,9 +1,5 @@
 import styled from "styled-components";
 
 export const QrWrap = styled.div`
-  /* pointer-events: none; */
-
-  @media (max-width: 25rem) {
-    margin: 16px -12px;
-  }
+  pointer-events: none;
 `;
