181eadc47674e5b48dd462543e492b59a0946b2b
nenadmarinkovic <nenadmarinkovic@protonmail.com>
2023-01-23 21:01:03 +0100

    layout changes

--- components/Panel.tsx
+++ components/Panel.tsx
@@ -3,7 +3,9 @@ import {
   PanelWrap,
   Title,
   Text,
+  PanelButton,
 } from "../styles/components/panel";
+import Link from "next/link";
 
 function Panel() {
   return (
@@ -12,9 +14,12 @@ function Panel() {
         <Title> Let&apos;s build together.</Title>
         <Text>
           Web is an unique platform for creativity, businesses, and growth.
-          Let&apos;s combine your idea with my technical know-how to build amazing
-          projects. Together.
+          Let&apos;s combine your idea with my technical know-how to build
+          amazing projects. Together.
         </Text>
+        <Link href="/contact">
+          <PanelButton>Contact</PanelButton>
+        </Link>
       </PanelInside>
     </PanelWrap>
   );
--- styles/components/panel.ts
+++ styles/components/panel.ts
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const PanelWrap = styled.div`
   margin: 10.5rem 0;
-  padding: 200px 75px;
+  padding: 200px;
   border-radius: 1rem;
   position: relative;
   background: linear-gradient(
@@ -14,8 +14,7 @@ export const PanelWrap = styled.div`
 `;
 
 export const PanelInside = styled.div`
-  transition: 0.25s;
-  padding: 50px 110px;
+  padding: 80px;
   content: "";
   border-radius: 8px;
   background: ${({ theme }) => theme.background};
@@ -25,6 +24,9 @@ export const PanelInside = styled.div`
   width: calc(100% - 30px);
   height: calc(100% - 30px);
   z-index: 1;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
 
   @media (max-width: 48rem) {
     padding: 50px 20px;
@@ -35,10 +37,24 @@ export const Title = styled.h1`
   font-size: 2.5rem;
   text-align: center;
   color: ${({ theme }) => theme.color};
+  margin: 0;
 `;
 
 export const Text = styled.p`
   font-size: 1.15rem;
   text-align: center;
   color: ${({ theme }) => theme.color};
+`;
+
+export const PanelButton = styled.button`
+  background: ${({ theme }) => theme.activeButton};
+  padding: 0.65em 1.8em;
+  color: ${({ theme }) => theme.activeButtonColor};
+  border: 1px solid ${({ theme }) => theme.activeButtonColor};
+  border-radius: 10px;
+  cursor: pointer;
+  line-height: 1;
+  font-size: 1rem;
+  letter-spacing: 0.6px;
+  font-weight: bold;
 `;
