48a557da7385c9b0df2154466ef4a25d2a6602d9
nenadmarinkovic <nenadmarinkovic@protonmail.com>
2023-03-02 11:55:49 +0100

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -45,21 +45,21 @@ class Doc extends Document {
           <link href="/manifest.json" rel="manifest" />
           <link href="/images/apple-touch-icon.png" rel="apple-touch-icon" />
           <link
-            rel="stylesheet preload"
+            rel="stylesheet prefetch"
             href="/fonts/CeraPro-Regular.woff2"
             as="font"
             type="font/woff2"
             crossOrigin="anonymous"
           />
           <link
-            rel="stylesheet preload"
+            rel="stylesheet prefetch"
             href="/fonts/CeraPro-Bold.woff2"
             as="font"
             type="font/woff2"
             crossOrigin="anonymous"
           />
           <link
-            rel="stylesheet preload"
+            rel="stylesheet prefetch"
             href="/fonts/CeraPro-Black.woff2"
             as="font"
             type="font/woff2"
