6ee4db95967e52ed498c6841366d98fa37bb34c0
Nenad Marinković <nenadmarinkovic@protonmail.com>
2023-05-24 07:50:46 +0200

    Update header.ts

--- styles/components/header.ts
+++ styles/components/header.ts
@@ -4,7 +4,7 @@ export const HeaderWrap = styled.header`
   position: sticky;
   top: 0;
   width: 100%;
-  padding: 1.2rem 0 1rem 0;
+  padding: 1.15rem 0 1rem 0;
   border-bottom: ${({ theme }) => theme.headerAndMenuBorder};
   background: ${({ theme }) => theme.headerBackground};
   backdrop-filter: saturate(70%) blur(3px);
