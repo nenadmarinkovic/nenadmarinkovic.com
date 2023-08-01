4724e3cf3441a8a7e64a4a1f67c8c35fcc0171cb
Nenad Marinković <nenadmarinkovic@protonmail.com>
2023-08-01 20:08:43 +0200

    deleted metrics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,15 +3,10 @@ import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "../styles/global";
 import { useTheme } from "../hooks/useTheme";
 import { lightTheme, darkTheme } from "../styles/theme";
-import { useReportWebVitals } from "next/web-vitals";
 
 export default function App({ Component, pageProps }: AppProps) {
   const [theme, toggleTheme, componentMounted] = useTheme();
   const themeMode = theme === "light" ? lightTheme : darkTheme;
-
-  useReportWebVitals((metric) => {
-    console.log(metric);
-  });
 
   if (!componentMounted) {
     return <div />;
