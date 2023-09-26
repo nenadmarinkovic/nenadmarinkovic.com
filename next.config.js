9d8173c7ca4b6dc2b56b8d117218d07b1f1f1bcb
Nenad Marinković <nenadmarinkovic@protonmail.com>
2023-09-26 22:29:51 +0200

    update config

--- next.config.js
+++ next.config.js
@@ -8,6 +8,8 @@ module.exports = withImages({
   reactStrictMode: true,
   swcMinify: true,
   pageExtensions: ["ts", "tsx", "mdx"],
+  assetPrefix: 'https://nenadmarinkovic.com',
+  dynamicAssetPrefix: true,
 
   images: {
     domains: ["i.scdn.co"],
